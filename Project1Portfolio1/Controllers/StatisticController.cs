aa400a78a70c59fd325ee200467d4e1f2105775e
FatmaBuseBorlu <fatmabuseborlu@gmail.com>
2024-10-01 20:16:18 +0300

    features

---
+++ Project1Portfolio1/Controllers/StatisticController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project1Portfolio1.Models;
+
+namespace Project1Portfolio1.Controllers
+{
+    public class StatisticController : Controller
+    {
+        MyPortflıoEntities1 context = new MyPortflıoEntities1();
+
+        public ActionResult Index()
+        {
+            ViewBag.totalMessageCount = context.Profile.Count();
+            ViewBag.messageIsReadTrue
+
+            return View();
+        }
+    }
+}
